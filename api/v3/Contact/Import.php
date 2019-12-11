03db139c9bb255e157abf8ca2a1e7130c7d3f333
dusan <dgeldner@josiahventure.com>
2019-12-11 14:33:03 +0100

    Full load bef DEC sprint

--- api/v3/Contact/Import.php
+++ api/v3/Contact/Import.php
@@ -22,72 +22,171 @@ function _civicrm_api3_contact_Import_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_contact_Import($params) {
-   $sql = <<<SQL
+    $sql = <<<SQL
           select const_key, ext_id, contact_type, contact_subtp, rcg_name, first_name, last_name, org_name, gender_id, birth_dt, email, phone, addr1, city, zip_cd, addr_cntry, src_sys_id, trg_id
-          from const 
-          where contact_subtp='Denomination'
+          from int_const 
+          /*where org_name like 'AC%'
+          limit 7*/
+          /*where contact_subtp='Denomination'*/
          
 SQL
-;
+    ;
 
-   $dao = CRM_Core_DAO::executeQuery($sql);
-   while($dao->fetch()){
+    $dao = CRM_Core_DAO::executeQuery($sql);
 
-     try{
+    while($dao->fetch()) {
 
-      $apiParams =  [
-        'external_identifier' => $dao->ext_id,
-        'contact_type'        => $dao->contact_type,
-        'contact_sub_type'    => $dao->contact_subtp,
-        'first_name'          => $dao->first_name,
-        'last_name'           => $dao->last_name,
-        'organization_name'   => $dao->org_name,
-        'gender_id'          =>  $dao->gender_id,
-        'birth_date'         =>  $dao->birth_dt
-      ];
+        if ($dao->trg_id == 0) {
+            try{
 
-      print_r($apiParams);
+                $apiParams =  [
+                    'external_identifier' => $dao->ext_id,
+                    'contact_type'        => $dao->contact_type,
+                    'contact_sub_type'    => $dao->contact_subtp,
+                    'first_name'          => $dao->first_name,
+                    'last_name'           => $dao->last_name,
+                    'legal_name'          => $dao->org_name,
+                    'organization_name'   => $dao->org_name,
+                    'sort_name'           => $dao->rcg_name,
+                    'gender_id'           => $dao->gender_id,
+                    'birth_date'          => $dao->birth_dt
+                ];
 
-      if($dao->trg_id){
-        $apiParams['id'] = $dao->trg_id;
-      }
-      $result = civicrm_api3('Contact', 'create',$apiParams);
-     } catch (CiviCRM_API3_Exception $e){
-       echo "$dao->const_key {$e->getMessage()}\n";
-       continue;
-     }
+                // print_r($apiParams);
+                echo "$dao->const_key\n";
 
-     if($result['is_error']){
-       echo $result['error_message'] ."\n";
-       continue;
-     }
+                if($dao->trg_id){
+                    $apiParams['id'] = $dao->trg_id;
+                }
+                $result = civicrm_api3('Contact', 'create',$apiParams);
+            }
+            catch (CiviCRM_API3_Exception $e) {
+                echo "$dao->const_key {$e->getMessage()}\n";
+                continue;
+            }
 
-     $contactId = $result['id'];
+        } else {
 
-     CRM_Core_DAO::executeQuery('update const set trg_id = %1 where const_key=%2',[
-       1 => [$contactId,'Integer'],
-       2 => [$dao->const_key,'String']
-     ]);
+            try{
 
-     if(isset($dao->email)){
+                $apiParams =  [
+                    'id'                  => $dao->trg_id,
+                    'external_identifier' => $dao->ext_id,
+                    'contact_type'        => $dao->contact_type,
+                    'contact_sub_type'    => $dao->contact_subtp,
+                    'first_name'          => $dao->first_name,
+                    'last_name'           => $dao->last_name,
+                    'legal_name'          => $dao->org_name,
+                    'organization_name'   => $dao->org_name,
+                    'sort_name'           => $dao->rcg_name,
+                    'gender_id'           => $dao->gender_id,
+                    'birth_date'          => $dao->birth_dt
+                ];
 
-       $apiParams = [
-         'contact_id' => $contactId,
-         'email'      => $dao->email,
-       ];
+                // print_r($apiParams);
+                echo "$dao->const_key\n";
 
-       $email_id = CRM_Core_DAO::singleValueQuery('select id from civicrm_email where contact_id=%1',
-         [
-           1=> [$contactId,'Integer'],
-         ]
-       );
+                $result = civicrm_api3('Contact', 'create',$apiParams);
+            }
 
-       if($email_id){
-         $apiParams['id'] = $email_id;
-       }
-       civicrm_api3('Email','create',$apiParams);
-     }
+            catch (CiviCRM_API3_Exception $e) {
+                echo "$dao->const_key {$e->getMessage()}\n";
+                CRM_Core_DAO::executeQuery('INSERT INTO int_log (TBL, REC_KEY, MSG, INS_DT) VALUES (%1, %2, %3, now())',
+                [
+                    1 => ['CONT','String'],
+                    2 => [$dao->const_key,'Integer'],
+                    3 => [$e->getMessage(),'String']
+                ]);
+                continue;
+            }
 
-   };
+        }
+
+        if($result['is_error']){
+            echo $result['error_message'] ."\n";
+            continue;
+        }
+
+        $contactId = $result['id'];
+        echo 'Contact id:'.$contactId."\n";
+        CRM_Core_DAO::executeQuery('update int_const set trg_id = %1 where const_key=%2',[
+            1 => [$contactId,'Integer'],
+            2 => [$dao->const_key,'String']
+        ]);
+
+        if(isset($dao->email)){
+
+            try{
+            $apiParams = [
+                'contact_id' => $contactId,
+                'email'      => $dao->email,
+            ];
+
+            $email_id = CRM_Core_DAO::singleValueQuery('select id from civicrm_email where contact_id=%1',
+                [
+                    1=> [$contactId,'Integer'],
+                ]
+            );
+
+            if($email_id){
+                $apiParams['id'] = $email_id;
+            }
+
+            $result = civicrm_api3('Email','create',$apiParams);
+            }
+
+            catch (CiviCRM_API3_Exception $e) {
+                echo "$dao->const_key {$e->getMessage()}\n";
+                CRM_Core_DAO::executeQuery('INSERT INTO int_log (TBL, REC_KEY, MSG, INS_DT) VALUES (%1, %2, %3, now())',
+                    [
+                        1 => ['EMAIL','String'],
+                        2 => [$dao->const_key,'Integer'],
+                        3 => [$e->getMessage(),'String']
+                    ]);
+                continue;
+            }
+            if($result['is_error']){
+              echo $result['error_message'] ."\n";
+              continue;
+            }
+
+        }
+
+        if(isset($dao->phone)){
+
+            try{
+            $apiParams = [
+                'contact_id' => $contactId,
+                'phone'      => $dao->phone,
+            ];
+
+            $phone_id = CRM_Core_DAO::singleValueQuery('select id from civicrm_phone where contact_id=%1',
+                [
+                    1=> [$contactId,'Integer'],
+                ]
+            );
+
+            if($phone_id){
+                $apiParams['id'] = $phone_id;
+            }
+            $result = civicrm_api3('Phone','create',$apiParams);
+            }
+            catch (CiviCRM_API3_Exception $e) {
+                echo "$dao->const_key {$e->getMessage()}\n";
+                CRM_Core_DAO::executeQuery('INSERT INTO int_log (TBL, REC_KEY, MSG, INS_DT) VALUES (%1, %2, %3, now())',
+                    [
+                        1 => ['PHONE','String'],
+                        2 => [$dao->const_key,'Integer'],
+                        3 => [$e->getMessage(),'String']
+                    ]);
+                continue;
+            }
+
+            if($result['is_error']){
+              echo $result['error_message'] ."\n";
+              continue;
+            }
+
+        }
+    }
 }
-
--- api/v3/Relationship/Import.php
+++ api/v3/Relationship/Import.php
@@ -24,46 +24,65 @@ function _civicrm_api3_relationship_Import_spec(&$spec) {
  */
 function civicrm_api3_relationship_Import($params) {
   $sql = <<<SQL
-          select const_key,church_const_key,REL_TP
-          from relationship;
+          select r.rel_key, r.frst_const_key, r.scnd_const_key, civi_tp, trg_id /* c1.trg_id, c2.trg_id, */
+          from int_rel r
+           /*inner join int_const c1 on c1.const_key = r.frst_const_key
+           inner join int_const c2 on c2.const_key = r.scnd_const_key*/
+           ;
 SQL
   ;
 
   $dao = CRM_Core_DAO::executeQuery($sql);
   while($dao->fetch()){
 
-     $contactId_A = CRM_CORE_DAO::singleValueQuery('select trg_id from const where const_key = %1',[
-       1 => [$dao->const_key,'Integer']
-     ]);
+    $contactId_A = CRM_CORE_DAO::singleValueQuery('select trg_id from int_const where const_key = %1',[
+      1 => [$dao->frst_const_key,'Integer']
+    ]);
 
-    $contactId_B = CRM_CORE_DAO::singleValueQuery('select trg_id from const where const_key = %1',[
-      1 => [$dao->church_const_key,'Integer']
+    $contactId_B = CRM_CORE_DAO::singleValueQuery('select trg_id from int_const where const_key = %1',[
+      1 => [$dao->scnd_const_key,'Integer']
     ]);
 
     $apiParams =  [
-      'contact_id_a' =>  $contactId_A,
+      'contact_id_a' => $contactId_A,
       'contact_id_b' => $contactId_B,
-      'relationship_type_id' => 15,
+      'relationship_type_id' => $dao->civi_tp
     ];
 
-    print_r($apiParams);
+    // print_r($apiParams);
 
     try{
       $result = civicrm_api3('Relationship', 'create', [
-        'contact_id_a' =>  $contactId_A,
+        'contact_id_a' => $contactId_A,
         'contact_id_b' => $contactId_B,
-        'relationship_type_id' => 15,
+        'relationship_type_id' => $dao->civi_tp /*%3*/
       ]);
 
-    } catch (CiviCRM_API3_Exception $e){
-    echo "$dao->const_key {$e->getMessage()}\n";
-    continue;
-  }
+      /*if($dao->trg_id){
+            $apiParams['id'] = $dao->trg_id;
+      }*/
+
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      echo "$dao->frst_const_key.$dao->scnd_const_key {$e->getMessage()}\n";
+      CRM_Core_DAO::executeQuery('INSERT INTO int_log (TBL, REC_KEY, MSG, INS_DT) VALUES (%1, %2, %3, now())',
+           [1 => ['REL','String'],
+            2 => [$dao->rel_key,'Integer'],
+            3 => [$e->getMessage(),'String']
+           ]);
+      continue;
+    }
 
     if($result['is_error']){
       echo $result['error_message'] ."\n";
       continue;
     }
 
+    $relationId = $result['id'];
+
+    CRM_Core_DAO::executeQuery('update int_rel set trg_id = %1 where rel_key=%2',[
+      1 => [$relationId,'Integer'],
+      2 => [$dao->rel_key,'Integer']
+    ]);
   }
 }
